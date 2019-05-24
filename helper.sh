497dec9b5300b122c8bc17264911c3a2a5fc2ff9
Benjamin EHLERS <Benjamin.EHLERS@hardis.fr>
2019-05-24 15:04:52 +0200

    First commit

---
+++ helper.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Variables 
+STDOUT_RED="\033[0;31m"
+STDOUT_YELLOW="\033[0;33m"
+STDOUT_DEFAULT="\033[0m"
+
+# generic
+#AWS_PROFILE="default"
+AWS_PROFILE="hardis"
+TEMPLATE_BUCKET="behlers-test1"
+PROJECT_NAME="testDMS"
+STACK_NAME="testDMS"
+TEMPLATE_NAME="root.yaml"
+
+usage()
+{
+    echo -e "${STDOUT_RED}$1"
+    echo -e "${STDOUT_YELLOW}Usage: $0  <upload | cb | validate | deploy>"
+    exit 1
+}
+
+upload()
+{
+    # aws s3 cp ./ s3://${TEMPLATE_BUCKET}/test1 --recursive  --exclude "root.yaml" --include "*.yaml" --profile ${AWS_PROFILE}
+    aws s3 cp ./ s3://${TEMPLATE_BUCKET}/testDMS --recursive  --exclude "*.sh" --exclude "*.json" --include "*.yaml" --exclude "root.yaml" --profile ${AWS_PROFILE}
+    # aws s3 cp ./StartScheduledInstances.zip s3://${TEMPLATE_BUCKET}/lambda/StartScheduledInstances.zip --profile ${AWS_PROFILE}
+    # aws s3 cp ./StopScheduledInstances.zip s3://${TEMPLATE_BUCKET}/lambda/StopScheduledInstances.zip --profile ${AWS_PROFILE}
+}
+
+create_bucket()
+{
+    aws s3 mb s3://${TEMPLATE_BUCKET} --profile ${AWS_PROFILE}
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -p|--profile)
+        export AWS_PROFILE=$2
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift # past command
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ $1 == "upload" ]; then 
+    # Upload Cfn templates to S3 bucket 
+    upload
+    exit $?
+fi
+
+if [ $1 == "test" ]; then 
+    # Upload Cfn templates to S3 bucket 
+    export engine=postgres
+    aws rds describe-db-engine-versions --query DBEngineVersions[?Engine==$engine].EngineVersion  --profile hardis
+    exit $?
+fi
+
+if [ $1 == "cb" ]; then 
+    # Create bucket 
+    create_bucket
+    exit $?
+fi
+
+if [ $1 == "validate" ]; then 
+    # Validate Root template
+    for filename in ./Cloudformation/*.yaml ; do
+        echo -e ${STDOUT_YELLOW}validating $filename template...${STDOUT_DEFAULT}
+        aws cloudformation validate-template --template-body file://$filename
+    done
+    exit $?
+fi
+
+if [ $1 == "deploy" ]; then 
+    # upload templates
+    upload
+
+    # Deploy stack
+    aws cloudformation deploy  \
+        --stack-name $STACK_NAME \
+        --template-file ./${TEMPLATE_NAME}  \
+        --parameter-overrides TemplateBucket=${TEMPLATE_BUCKET} \
+                              ProjectName=${PROJECT_NAME} \
+        --capabilities CAPABILITY_IAM \
+         --profile ${AWS_PROFILE} \
+
+    echo -e "\a"
+    exit $?
+fi
+
+if [ $1 == "delete" ]; then 
+
+    # Deploy stack
+    aws cloudformation delete-stack  \
+        --stack-name $STACK_NAME \
+        --profile ${AWS_PROFILE} \
+
+    echo -e "\a"
+    exit $?
+fi
+
+usage "Failed script execution!"
